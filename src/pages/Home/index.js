ff3fcd9dea893547e9d96802d699ac6f1b615e5e
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-08-31 20:35:57 -0300

    better css and load screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,8 +7,9 @@ import "./style.css";
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  async function loading() {
+  async function loadingfilmes() {
     const response = await api.get("movie/now_playing", {
       params: {
         api_key: "c634b4775a037a56f5dd2baf3951c4e8",
@@ -17,31 +18,50 @@ function Home() {
       },
     });
 
+    function load() {
+      setLoading(false);
+    }
+
     let finalFilms = response.data.results;
     setFilmes(finalFilms);
+    setTimeout(load, 5000);
 
     console.log(response.data.results);
   }
 
   useEffect(() => {
-    loading();
+    loadingfilmes();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="container">
+        {filmes.map((filmes) => (
+          <article className="falsolista" key={filmes.id}>
+            <strong className="falsotitulo">Carregando...</strong>
+            <div className="falsoimage" />
+          </article>
+        ))}
+      </div>
+    );
+  }
   return (
-    <div className="container">
-      {filmes.map((filmes) => (
-        <article className="lista" key={filmes.id}>
-          <strong className="titulo">{filmes.title}</strong>
-          <img
-            className="image"
-            src={`https://image.tmdb.org/t/p/w500/${filmes.poster_path}`}
-            alt={filmes.title}
-          />
-          <Link className="link" to={`/Filmes/${filmes.id}`}>
-            Acessar
-          </Link>
-        </article>
-      ))}
+    <div className="father">
+      <div className="container">
+        {filmes.map((filmes) => (
+          <article className="lista" key={filmes.id}>
+            <strong className="titulo">{filmes.title}</strong>
+            <img
+              className="image"
+              src={`https://image.tmdb.org/t/p/w500/${filmes.poster_path}`}
+              alt={filmes.title}
+            />
+            <Link className="link" to={`/Filmes/${filmes.id}`}>
+              Acessar
+            </Link>
+          </article>
+        ))}
+      </div>
     </div>
   );
 }
