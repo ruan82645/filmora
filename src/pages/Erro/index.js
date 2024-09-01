397a1dc9b57ea9958e7622238163cde6fe444a6e
Ruan Pablo <ruanpabloalves82645@gmail.com>
2024-09-01 18:21:32 -0300

    way better css and not found page created

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
+import fightclub from "../../Assets/pics/fightclub.jpg";
 
 function Erro() {
   return (
-    <div>
-      <h1>Opa, parece que não encontrei essa página</h1>
-      <Link to="/">Home</Link>
-      <Link to="/Filmes">Filmes</Link>
+    <div className="errorcontainer">
+      <h1 className="title">Opa, parece que essa página não existe!</h1>
+      <h3 className="subtitle">Volte e assista alguns filmes :)</h3>
+      <div className="retorno">
+        <img className="fightclub" src={fightclub} />
+        <Link to="/">
+          <h1 className="retornar">RETORNAR</h1>
+        </Link>
+      </div>
+      <h2 className="rule">
+        {" "}
+        "The first rule about fight club is: You do not talk about fight club."
+      </h2>
     </div>
   );
 }
